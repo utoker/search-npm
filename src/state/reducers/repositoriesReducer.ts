93001f887b3e2f6bcaccab24f1380dd7900de5d3
Umut Toker <utoker@gmail.com>
2022-01-19 07:38:47 -0500

    add initial state

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -2,26 +2,32 @@ import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
 interface RepositoriesState {
-	loading: boolean;
-	error: string | null;
-	data: string[];
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
-const reducer = (
-	state: RepositoriesState,
-	action: Action
-): RepositoriesState => {
-	switch (action.type) {
-		case ActionType.SEARCH_REPOSITORIES:
-			return { loading: true, error: null, data: [] };
-		case ActionType.SEARCH_REPOSITORIES_SUCCESS:
-			return { loading: false, error: null, data: action.payload };
-		case ActionType.SEARCH_REPOSITORIES_ERROR:
-			return { loading: false, error: action.payload, data: [] };
+const initialState: RepositoriesState = {
+  loading: false,
+  error: null,
+  data: [],
+};
 
-		default:
-			return state;
-	}
+const reducer = (
+  state: RepositoriesState = initialState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
