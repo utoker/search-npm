97f51cc7c79e0acbbd5a3712f2eeaab08ff49451
Umut Toker <utoker@gmail.com>
2022-01-19 12:14:12 -0500

    list packages

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -8,7 +8,6 @@ const RepositoriesList: React.FC = () => {
   const { data, error, loading } = useTypedSelector(
     (state) => state.repositories
   );
-  console.log(data, error, loading);
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     searchRepositories(term);
@@ -19,6 +18,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
